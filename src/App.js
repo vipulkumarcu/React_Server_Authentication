a3b2808187356373099f83eeb3c6817253c75c38
vipulkumar.gts@gmail.com <vipulkumar.gts@gmail.com>
2024-08-03 16:29:04 +0530

    Sh (20) - Addressing security and logout issues

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import { Switch, Route } from 'react-router-dom';
-
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import { useContext } from 'react';
+import AuthContext from './store/auth-context';
 
 function App() {
+  const context = useContext(AuthContext);
+  
   return (
     <Layout>
       <Switch>
         <Route path='/' exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
-          <AuthPage />
-        </Route>
+        {
+          !context.isLoggedIn && <Route path='/auth'> <AuthPage /> </Route>
+        }
         <Route path='/profile'>
-          <UserProfile />
+        { context.isLoggedIn && <UserProfile /> }
+        { !context.isLoggedIn && <Redirect to="/auth" /> }
+        </Route>
+
+        <Route path='*'>
+          <Redirect to="/" />
         </Route>
       </Switch>
     </Layout>
